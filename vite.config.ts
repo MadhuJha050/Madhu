d32e071e68893e458c65d0a4f5af41516a44c36f
MadhuJha050 <madhujha2050@gmail.com>
2025-08-13 10:14:01 +0545

    chnages

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/MJ-portfolio/',
+  base: '/Madhu/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
